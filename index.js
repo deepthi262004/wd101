b8fb40b119495b43ced6c6921d71474775e769be
deepthi262004 <129207832+deepthi262004@users.noreply.github.com>
2023-08-08 19:03:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const form = document.getElementById('registrationForm');
 const userTable = document.getElementById('userTable').querySelector('tbody');
 
@@ -7,15 +8,24 @@ form.addEventListener('submit', function(event) {
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
-  const dob = document.getElementById('dob').value;
+  const dobInput = document.getElementById('dob');
   const acceptedTerms = document.getElementById('acceptedTerms').checked;
+  
+  const dob = new Date(dobInput.value);
+  const today = new Date();
+  const age = today.getFullYear() - dob.getFullYear();
+  
+  if (age < 18 || age > 54) {
+    alert('DOB must be between 18 and 54 years old.');
+    return;
+  }
   
   const newRow = document.createElement('tr');
   newRow.innerHTML = `
     <td>${name}</td>
     <td>${email}</td>
     <td>${password}</td>
-    <td>${dob}</td>
+    <td>${dobInput.value}</td>
     <td>${acceptedTerms ? 'Yes' : 'No'}</td>
   `;
   
